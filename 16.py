25a292a279b28efc0155ca07d665d301b607f572
Ken <nukoug@gmail.com>
2013-04-22 10:20:39 +0800

    Project Euler

--- 16.py
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/python
-
-num = 2**1000
-print eval('+'.join(str(num)))
--- 25.py
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/python
-
-a,b,i = 1,1,1
-while len(str(a)) < 1000:
-  a,b = b,a+b
-  i = i + 1
-print (i)
